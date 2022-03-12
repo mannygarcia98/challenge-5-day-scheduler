bff5450009bcded148f10fa3561eedfd66c0920a
Manny <mannysg88@gmail.com>
2022-03-12 17:51:28 -0600

    added save button functionality and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,22 @@
-//1. add time to #currentDay
-//2. add ability to see if the hour has passed yet.
-//3. add function to add text to local storage.
-//4. add event listener to save button
+//✅ 1. add time to #currentDay
+//✅ 2. add ability to see if the hour has passed yet.
+//✅ 3. add event listener to save button
+//4. add corresponding key and value for local storage on button click
+// 5. create function to retrieve values from local storage
+//6. refactor code
 
 var currentDay = $("#currentDay");
-
 var rightNow = moment().format("dddd MMMM Do");
-console.log(rightNow);
 var currentHour = moment().hour();
-//test color block function
-// var currentHour = 12;
-console.log(typeof currentHour);
 
 (function writeDate() {
   currentDay.html(rightNow);
 })();
 
-//need an if statement that changes: the current timeblock red, the past blocks gray, and the future blocks green
-
+//add background color depending on time of day
 $(".time-block").each(function () {
-  console.log($(this).attr("id"));
+  //gets current time from id name dropping the 'hour'
   var timeBlockHour = $(this).attr("id").replace("hour", "");
-  console.log(timeBlockHour);
-
   //remove block color if any
   $(this).removeClass("past present future");
 
@@ -33,4 +27,16 @@ $(".time-block").each(function () {
   } else {
     $(this).addClass("future");
   }
+  //get values from local storage if any
+  //need a loop that will change just the hour and repeat for each time block
+  $("#hour" + timeBlockHour + " .description").val(localStorage.getItem("hour" + timeBlockHour));
+});
+
+//saves description to local storage on button click
+$(".saveBtn").on("click", function () {
+  var hour = $(this).parent().attr("id");
+  var description = $(this).siblings(".description").val();
+  console.log(hour);
+  console.log(description);
+  localStorage.setItem(hour, description);
 });
