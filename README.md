afb0439692afdf5cf185aa56fa1f3e32467d9490
Manny <mannysg88@gmail.com>
2022-03-12 13:56:36 -0600

    add background color function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work Day Scheduler Starter Code
+
+https://mannygarcia98.github.io/challenge-5-day-scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,34 @@
 //3. add function to add text to local storage.
 //4. add event listener to save button
 
-var currentDay = document.querySelector("#currentDay");
+var currentDay = $("#currentDay");
 
 var rightNow = moment().format("dddd MMMM Do");
 console.log(rightNow);
+var currentHour = moment().hour();
+//test color block function
+// var currentHour = 12;
+console.log(typeof currentHour);
 
 (function writeDate() {
-  currentDay.textContent = rightNow;
+  currentDay.html(rightNow);
 })();
+
+//need an if statement that changes: the current timeblock red, the past blocks gray, and the future blocks green
+
+$(".time-block").each(function () {
+  console.log($(this).attr("id"));
+  var timeBlockHour = $(this).attr("id").replace("hour", "");
+  console.log(timeBlockHour);
+
+  //remove block color if any
+  $(this).removeClass("past present future");
+
+  if (timeBlockHour == currentHour) {
+    $(this).addClass("present");
+  } else if (timeBlockHour < currentHour) {
+    $(this).addClass("past");
+  } else {
+    $(this).addClass("future");
+  }
+});
